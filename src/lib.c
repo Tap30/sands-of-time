c93702ef36d2c8dc9d2c8bac42f2ed40047f21fe
Mohamad Reza Lotfi Namin <mohamadmlnnamin@gmail.com>
2018-05-29 21:23:13 +0430

    Add todo comments

--- src/lib.c
+++ src/lib.c
@@ -9,17 +9,19 @@ void init() {
 }
 
 int clock_gettime(clockid_t clk_id, struct timespec *tp) {
+    // TODO call real time and do timeshift
     tp->tv_nsec = 0;
     tp->tv_sec = 1500000;
     return 0;
 }
 
 int gettimeofday(struct timeval *restrict tp, void *restrict tzp) {
+    // TODO call real time and do timeshift
     return 1500000;
 }
 
 
-
+// TODO implment these too
 // time_t time(time_t *tloc) {
 //     return 1500000;
 // }
