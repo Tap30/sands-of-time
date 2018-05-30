8ca28e6de0aa9e2476f16f089577694fbdf2f75a
Mohamad Reza Lotfi Namin <mohamadmlnnamin@gmail.com>
2018-05-30 13:58:40 +0430

    Add signal handler

--- src/lib.c
+++ src/lib.c
@@ -1,21 +1,35 @@
+#include <signal.h>
+
 #include <sys/types.h>
 #include <time.h>
 #include <sys/timeb.h>
-// static boolean initialized = false;
 
 static time_t       (*real_time)            (time_t *);
 static int          (*real_ftime)           (struct timeb *);
 static int          (*real_gettimeofday)    (struct timeval *, void *);
 static int          (*real_clock_gettime)   (clockid_t clk_id, struct timespec *tp);
 
+static int initialized = 0;
 static int specifiedtime = 1600000;
 
+void usr_signal_handler(int signum) {
+    printf("Caught signal %d\n",signum);
+    // Cleanup and close up stuff here
+    int * a;
+    a = 0;
+    *a = 12313;
+}
 
 void init() {
-
+    printf("init");
+    signal(SIGUSR2, usr_signal_handler);
+    initialized = 1;
 }
 
 int clock_gettime(clockid_t clk_id, struct timespec *tp) {
+    if (!initialized) {
+        init();
+    }
     // TODO call real time and do timeshift
     tp->tv_nsec = 0;
     tp->tv_sec = specifiedtime;
@@ -23,6 +37,9 @@ int clock_gettime(clockid_t clk_id, struct timespec *tp) {
 }
 
 int gettimeofday(struct timeval *tp, void *tz) {
+    if (!initialized) {
+        init();
+    }
     // TODO call real time and do timeshift
     tp->tv_sec = specifiedtime;
     tp->tv_usec = 0;
@@ -30,6 +47,9 @@ int gettimeofday(struct timeval *tp, void *tz) {
 }
 
 time_t time(time_t *tloc) {
+    if (!initialized) {
+        init();
+    }
     if (tloc != NULL) {
         *tloc = specifiedtime;
     }
@@ -37,6 +57,9 @@ time_t time(time_t *tloc) {
 }
 
 int ftime(struct timeb * s) {
+    if (!initialized) {
+        init();
+    }
     s->time = specifiedtime;
     s->millitm = 0;
 
