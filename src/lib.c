55dcb385f4f274ccd9523cd6c008da4cab2cef2d
Mohamad Reza Lotfi Namin <mohamadmlnnamin@gmail.com>
2018-05-30 13:21:08 +0430

    Fix function implementations

--- src/lib.c
+++ src/lib.c
@@ -17,15 +17,21 @@ int clock_gettime(clockid_t clk_id, struct timespec *tp) {
 
 int gettimeofday(struct timeval *restrict tp, void *restrict tzp) {
     // TODO call real time and do timeshift
+    tp->tv_sec = 1500000;
+    tp->tv_usec = 0;
+    return 0;
+}
+
+time_t time(time_t *tloc) {
+    if (tloc != NULL) {
+        *tloc = 1500000;
+    }
     return 1500000;
 }
 
+int ftime(struct timeb * s) {
+    s->time = 1500000;
+    s->millitm = 0;
 
-// TODO implment these too
-// time_t time(time_t *tloc) {
-//     return 1500000;
-// }
-
-// int ftime(struct timeb * s) {
-//     return 1500000;
-// }
+    return 0;
+}
