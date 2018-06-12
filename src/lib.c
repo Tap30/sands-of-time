1a0c7901f48259c247e3aa69e5692613c5bbdc66
Mohamad Reza Lotfi Namin <mohamadmlnnamin@gmail.com>
2018-06-12 18:33:06 +0430

    Fix arrow pointer access

--- src/lib.c
+++ src/lib.c
@@ -43,8 +43,10 @@ int 				get_fake_time				(struct fake_time *time);
 
 int get_fake_time(struct fake_time *time)
 {
-	time.seconds = 0;
-	time.microseconds = 0;
+    fake_time_alpha = 0;
+    fake_time_beta = 0;
+	time->seconds = 0;
+	time->microseconds = 0;
 	return 0;
 }
 
